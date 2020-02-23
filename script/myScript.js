2707c5d5f2631861617a587d9f9340f5f443e26a
Rosheen Naeem <rosheen@xgrid.co>
2020-02-23 12:57:13 +0500

    TT website first commit

---
+++ script/myScript.js
@@ -0,0 +1,19 @@
+function contactDisplay1(){
+
+	document.getElementById('contactNo').innerHTML = "+924564566";
+}
+
+
+function contactDisplay2(){
+	document.getElementById('contactNo2').innerHTML = "+924564566";
+}
+
+
+function bigImg(x) {
+  x.style.height = "264px";
+}
+
+function normalImg(x) {
+  x.style.height = "235px";
+}
+
