7fe5138e6ee8e866adb731053d58a39c41426aaf
ROSHEEN NAEEM <rosheennaeem4@gmail.com>
2020-03-23 16:15:36 +0500

    Add files via upload

---
+++ About.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About</title>
+	<link rel="stylesheet" type="text/css" href="css/about.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="script/myScript.js"></script>
+</head>
+<body>
+
+<div class="logo" style="padding: 0.1%;"  >
+	<span style="text-align: center; padding: 1%;">TT Interior designer</span>
+	<a href= "signUp.php"  style="float: right; font-size: 30px; margin-right: 1%; color: white;">SignUp</a> 
+
+	<a href= "signIn.php"  style="float: right; font-size: 30px; margin-right: 1%; color: white;">Login</a>
+
+	
+	
+
+</div>
+
+
+<!-- Navigation Bar -->
+	<ul>
+	  <li><a class="active nav" href="index.php">Home</a></li>
+	  <li><a class="nav" href="About.php">About</a></li>
+	  <li><a class="nav" href="Services.php">Services</a></li>
+	  <li><a class="nav" href="Products.php">Products</a></li>
+	  <li><a class="nav" href="Contact.php">Contact</a></li>
+	  <li><a class="nav" href="Material.php">Material</a></li>
+	  
+
+  
+    </ul>
+
+
+    <h1> About Us </h1>
+
+     <div class="aboutusinfo">
+			<div class="imagepersonleft">
+				<img class="imageperson" src="images/value.jpg">
+			</div>
+			<div class="personinfoleft">
+				<h2>Values</h2>
+				<p> An encounter that sets the senses free and traces the colour of every day. The pieces we create at <b>TT</b> are unique masterpieces that cherish within their souls a destination and all the emotions lived during the travel towards their realization.
+				</p>
+			</div>
+			<div style="clear:both"></div>
+		 </div>
+
+		 <div class="aboutusinfo">
+			<div class="imagepersonright">
+				<img class="imageperson" src="images/brand.jpg">
+			</div>
+			<div class="personinforight">
+				<h2>Brand</h2>
+				<p> A journey begun looking at our native land with new eyes, seemingly suspended in a timeless dimension and untouched by modernity, but which appears to the patient observer as being authentic and of excellent craftsmanship handed down from one generation to the next, the highest expression of Italian know-how.
+				</p>
+			</div>
+			<div style="clear:both"></div>
+		 </div>
+
+		 <div class="aboutusinfo">
+		 <div class="imagepersonleft">
+				<img class="imageperson" src="images/philo.jpg">
+			</div>
+			<div class="personinfoleft">
+				<h2>Philosophy </h2>
+				<p> The philosophy that has led  <b>TT</b> since its dawn lays in the negation of these serial and industrial principles, enslaving our lives: at  <b>TT</b>, we create a design that embraces the total uniqueness, the idea of connecting each client and each experience not with a piece of furniture, yet with a traveling companion.
+				</p>
+			</div>
+			<div style="clear:both"></div>
+		 </div>
+		 <br><br><br>
+
+				<!-- footer -->
+<div class="footer">
+
+	<ul>
+		<li >
+			<a href="Privacy.php" class="foot"><u>Privacy Policy</u></a>
+		</li>
+		<li >
+			<a href="FAQs.php" class="foot"><u>FAQs</u></a>
+		</li>
+	</ul>
+</div>
+
+</body>
+</html>
