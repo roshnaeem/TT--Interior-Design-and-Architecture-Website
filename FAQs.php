2e1353debefd48c819aa51533a92f81c06b1bdf7
ROSHEEN NAEEM <rosheennaeem4@gmail.com>
2020-03-23 16:17:11 +0500

    Add files via upload

---
+++ FAQs.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/about.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="script/myScript.js"></script>
+</head>
+<body>
+<div class="logo" style="padding: 0.1%;"  >
+	<span style="text-align: center; padding: 1%;">TT Interior designer</span>
+	<a href= "signUp.php"  style="float: right; font-size: 30px; margin-right: 1%; color: white;">SignUp</a> 
+
+	<a href= "signIn.php"  style="float: right; font-size: 30px; margin-right: 1%; color: white;">Login</a>
+
+	
+	
+
+</div>
+
+
+<!-- Navigation Bar -->
+
+		<ul>
+	  <li><a class="active nav" href="index.php">Home</a></li>
+	  <li><a class="nav" href="About.php">About</a></li>
+	  <li><a class="nav" href="Services.php">Services</a></li>
+	  <li><a class="nav" href="Products.php">Products</a></li>
+	  <li><a class="nav" href="Contact.php">Contact</a></li>
+	  <li><a class="nav" href="Material.php">Material</a></li>
+	  
+
+  
+    </ul>
+
+    <h1> Frequently Asked Questions </h1>
+
+    <p class="question">
+    	I have a remodeling project but I’m not sure if I need an interior designer or an architect?
+    </p>
+    <p class="answer">
+    	This is a common question. Typically, an interior designer has the skills and professional education to design and draft new space plans — whether reconfiguring an existing interior space or adding square footage to a home as well as develop lighting and electrical plans, select all interior finishes such as flooring, countertops, tile, paint colors, wall and window treatments, as well as specify plumbing, appliances, cabinetry and furnishings, to name a few.
+
+		If your project involves changing structural load bearing walls, or adding square footage, an architect and/or structural engineer will need to be part of your team. They’ll provide the necessary drawings and structural calculations required by your local building department to ensure your safety and welfare, as well as pass building code inspections
+    </p>
+
+    <p class="question">
+    	 My project only involves re-decorating my space, how does a professional interior designer’s skills differ from an in-store decorator at a local furniture, tile, or kitchen and bath showroom?
+    </p>
+    <p class="answer">
+    	Many times retail in-store decorators are primarily sales people. They’ve had only a limited education in interior design and space planning, unlike professional interior designers who attend comprehensive, accredited 3-4 year interior design programs at colleges.
+
+		Typically, in-store decorators are not as well versed in scale, proportion, space planning, color theory and health and safety issues. Also, they’ll only offer you product selections from their featured lines. Professional interior designers have access to hundreds of trade-only vendors and manufacturer’s world wide. If you decide to use an in-store designer be sure to ask about their qualifications to determine if they’re the right match for your job.
+    </p>
+
+      <p class="question">
+    	  I would like to re-design my home or office but I don’t have a lot of time to devote to the process, should I still hire a designer?
+    </p>
+    <p class="answer">
+    	Yes, most definitely, we’ll handle the shopping, ordering and delivery of finish materials, furnishings and fixtures, as well as coordinate and oversee the labor so you can spend more time enjoying family, friends, loved ones and outside activities.
+
+		We’ll bring selections to your home or office if you prefer not to go to design showrooms with us and try to make it as convenient as possible. All we ask in return is that you respect our professional time and communicate with us in a timely fashion so we can help you create your vision.
+    </p>
+
+     <p class="question">
+    	   I don’t know what my design style is or how to articulate it, should I still work with an interior designer?
+    </p>
+    <p class="answer">
+    	We hear this concern frequently. Rest assured we have a unique assessment system to draw out what colors, textures, images, shapes, finishes and feelings resonate with you. Your answers inform our design process.
+
+		Our favorite part of our job is helping you create a space that’s completely unique to your personality and not a replica of “specific style” that looks done and impersonal.
+	</p>
+
+	 <p class="question">
+    	   What is TT Interiors Design’s style?
+    </p>
+    <p class="answer">
+    	We don’t subscribe to a just one particular design style such as modern, transitional or traditional. As creative professionals we enjoy working on a variety of project styles from elegant traditional kitchens, to soft-modern bathrooms, to mid-century family rooms, to name a few. This keeps business fresh and interesting. As such, we are always seeking out new artisans and vendors to help create new, custom pieces for you that complement your personalized style that we co-create with you.
+	</p>
+
+	 <p class="question">
+    	   What other services does TT Interiors Design offer clients?
+    </p>
+    <p class="answer">
+    	We want to save time you and give you peace of mind by acting as your “go to” resource for everything related to the design, as well as the care and maintenance of your home or private office.
+
+		From rug cleaning to re-finishing floors and re-sealing countertops to removing junk, we will keep your home or office looking fabulous and functioning well.
+	</p>
+	<br><br>
+		<!-- footer -->
+<div class="footer">
+
+	<ul>
+		<li >
+			<a href="Privacy.php" class="foot"><u>Privacy Policy</u></a>
+		</li>
+		<li >
+			<a href="FAQs.php" class="foot"><u>FAQs</u></a>
+		</li>
+	</ul>
+</div>
+
+</body>
+</html>
