8a7bc82140bd61ba8b298d5eb69a0c11ff86b5eb
ROSHEEN NAEEM <rosheennaeem4@gmail.com>
2020-03-23 16:16:40 +0500

    Add files via upload

---
+++ Contact.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HomePage</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="script/myScript.js"></script>
+</head>
+<body>
+
+<div class="logo" style="padding: 0.1%;"  >
+	<span style="text-align: center; padding: 1%;">TT Interior designer</span>
+	<a href= "signUp.php"  style="float: right; font-size: 30px; margin-right: 1%; color: white;">SignUp</a> 
+
+	<a href= "signIn.php"  style="float: right; font-size: 30px; margin-right: 1%; color: white;">Login</a>
+
+	
+	
+
+</div>
+
+
+<!-- Navigation Bar -->
+
+		<ul>
+	  <li><a class="active nav" href="index.php">Home</a></li>
+	  <li><a class="nav" href="About.php">About</a></li>
+	  <li><a class="nav" href="Services.php">Services</a></li>
+	  <li><a class="nav" href="Products.php">Products</a></li>
+	  <li><a class="nav" href="Contact.php">Contact</a></li>
+	  <li><a class="nav" href="Material.php">Material</a></li>
+	  
+
+  
+    </ul>
+
+ <h1> Contact Us</h1>
+
+		 <h2>Contact Information & Customer Service</h2>
+		 <hr>
+
+		<table style="width:100%; margin: 1% 1% 1% 0%; padding:0; border: 0;">
+			
+			<tr >
+				<td style="width:50%;"><h2> Contact Information</h2></td>
+				<td style="width:50%;"> <h2>Customer Service</h2></td>
+			</tr>
+			<tr>
+				<td>
+					<h3>First Class Interior Designing Market</h3><br>
+					United States of America <br>USA<br>
+			
+					<h3>Ready to Book?</h3>
+					<b>Email: telfah.mis86@yahoo.ca  </b><br>
+					For all other inquiries,
+					please email: telfah.mis86@yahoo.ca <br>
+					<h3> Current Hours</h3>
+					Monday-Friday 9am-5pm <br>
+					Saturday and Sunday 11am-8pm
+
+				</td>
+				<td ><p>
+					In order to respond more timely and accurately to your inquiries, please choose the category that most closely resembles your requirment. 
+					You can find all type of laptops on our website.
+				</p><br>
+					<h3>General Inquiries</h3><br>
+					For general inquiries, please email: telfah.mis86@yahoo.ca<br> 
+					<b style="color: #cc0000;">Tariq Talafha<br> </b>
+					<button type="button" class="contactButton"
+					onclick= "contactDisplay1()">Contact Info: 
+					</button>
+					<p id="contactNo"></p> <br>
+					<b style="color: #cc0000;">Tariq Talafha<br> </b>
+					<button type="button" class="contactButton"
+					onclick= "contactDisplay2()">Contact Info: 
+					</button>
+					<p id="contactNo2"></p> <br>
+					
+
+
+
+				</td>
+
+
+			</tr>
+		</table>
+
+		 <hr>
+
+		<div>
+			<h1>At your Service</h1>
+			<p>You will be impressed by our professionalism, the accuracy of the provided information, our prices and our reasonnable timeframes.</p>
+
+		</div>
+		 <br><br><br>
+
+				<!-- footer -->
+<div class="footer">
+
+	<ul>
+		<li >
+			<a href="Privacy.php" class="foot"><u>Privacy Policy</u></a>
+		</li>
+		<li >
+			<a href="FAQs.php" class="foot"><u>FAQs</u></a>
+		</li>
+	</ul>
+</div>
+</body>
