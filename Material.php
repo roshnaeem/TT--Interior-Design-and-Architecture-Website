43252a2c4629088666560b87e5360d3805a1edfe
ROSHEEN NAEEM <rosheennaeem4@gmail.com>
2020-03-24 16:48:45 +0500

    Add files via upload

---
+++ Material.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Designer</title>
+	<link rel="stylesheet" type="text/css" href="css/about.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="script/myScript.js"></script>
+</head>
+<body>
+
+<div class="logo" style="padding: 0.1%;"  >
+	<span style="text-align: center; padding: 1%;">TT Interior designer</span>
+	<a href= "signUp.php"  style="float: right; font-size: 30px; margin-right: 1%; color: white;">SignUp</a> 
+
+	<a href= "signIn.php"  style="float: right; font-size: 30px; margin-right: 1%; color: white;">Login</a>
+
+	
+	
+
+</div>
+
+
+<!-- Navigation Bar -->
+
+	<ul>
+	  <li><a class="active nav" href="index.php">Home</a></li>
+	  <li><a class="nav" href="About.php">About</a></li>
+	  <li><a class="nav" href="Services.php">Services</a></li>
+	  <li><a class="nav" href="Products.php">Products</a></li>
+	  <li><a class="nav" href="Contact.php">Contact</a></li>
+	  <li><a class="nav" href="Material.php">Material</a></li>
+	  
+
+  
+    </ul>
+
+<h1>Materials</h1>
+<p>Materials used by TT for manufacturing strong and reliable products. </p>
+<br><br>
+
+<div class="row">
+  <div class="column">
+    <img src="images/fabric.JPG" style="width:100%">
+  </div>
+  
+  <div class="column">
+    <img src="images/leather.JPG"   style="width:100%">
+  </div>
+  <div class="column">
+    <img src="images/metal.JPG"  style="width:100%">
+  </div>
+</div>
+<div class="row">
+<div class="column" style ="text-align:center;"><b style="color: #cc0000; font-size: 26px;">Fabric</b></div>
+<div class="column" style ="text-align:center;">Leather</div>
+<div class="column" style ="text-align:center;"><b style="color: #cc0000; font-size: 26px;">Metal</b></div>
+</div>
+<br><br><br>
+
+
+<div class="row">
+  <div class="column">
+    <img src="images/stone.JPG" style="width:100%">
+  </div>
+  
+  <div class="column">
+    <img src="images/cotton.jpg"  style="width:100%">
+  </div>
+  <div class="column">
+    <img src="images/wood.JPG"  style="width:100%">
+  </div>
+</div>
+<div class="row">
+<div class="column" style ="text-align:center;"><b style="color: #cc0000; font-size: 26px;">Stone</b></div>
+<div class="column" style ="text-align:center;">Cotton</div>
+<div class="column" style ="text-align:center;"><b style="color: #cc0000; font-size: 26px;">Wood</b></div>
+</div>
+<br><br><br>
+
+
+			<!-- footer -->
+<div class="footer">
+
+	<ul>
+		<li >
+			<a href="Privacy.php" class="foot"><u>Privacy Policy</u></a>
+		</li>
+		<li >
+			<a href="FAQs.php" class="foot"><u>FAQs</u></a>
+		</li>
+	</ul>
+</div>
+
+</body>
+</html>
